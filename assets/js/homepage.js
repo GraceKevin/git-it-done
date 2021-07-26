4254731b2ca03445e383cd8c4a88d46e350fdf27
Kevin Grace <Kevin@kevins-air.home>
2021-07-25 22:01:53 -0400

    updated js

---
+++ assets/js/homepage.js
@@ -0,0 +1,17 @@
+fetch("https://api.github.com/users/octocat/repos").then(function(response){
+        response.json().then(function(data) {
+            console.log(data);
+        });
+});
+
+var getUserRepos = function(user) {
+    // Format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // Make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log (data);
+        });
+    });
+};
